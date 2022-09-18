f144a61e1a4fcbe91d1284e1bde11b803d58b914
Paloma-P <palomapeterd9@gmail.com>
2022-09-17 21:35:29 -0700

    Front end for RSVP dapp

---
+++ utils/connectContract.js
@@ -0,0 +1,27 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0x003e7A7EE4E37F0e8dCf87f655ca2A77E029A3D2";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
+  
